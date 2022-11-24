9f0e58a00bf31c8eafd636384be5404d7ea97c6f
Benjamin Wagner <bwagner@vapita.de>
2022-11-24 07:39:59 +0100

    add ShopProduct Class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import product.ShopProduct;
+
 public class Main {
 
     public static void main(String[] args)
     {
+        ShopProduct product = new ShopProduct();
         System.out.println("Hello world!");
     }
 
