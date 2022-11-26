30f4d5b7b27876a49b5b7b8ad7b8034e4d4ee549
Benjamin Wagner <bwagner@vapita.de>
2022-11-26 11:41:59 +0100

    add JFrame

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import product.BookProduct;
 import product.CdProduct;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args)
@@ -25,8 +28,22 @@ public class Main {
 
         cd1.setAuthorName("Pentatonix");
 
-        System.out.println(book1.getSummaryLine());
-        System.out.println(cd1.getSummaryLine());
+        JFrame f = new JFrame("Medienbibliothek");
+        String[] data = {book1.getSummaryLine(),cd1.getSummaryLine()};
+        f.add(new JList<>(data));
+        f.pack();
+        f.setSize(800,600);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        JMenuBar menubar = new JMenuBar();
+        JMenu menu = new JMenu("Menu");
+        JMenuItem settings = new JMenuItem("Settings");
+        settings.addActionListener(e -> System.out.println("Open Settings"));
+        menu.add(settings);
+        menubar.add(menu);
+        f.setJMenuBar(menubar);
+        f.setVisible(true);
     }
 
 }
